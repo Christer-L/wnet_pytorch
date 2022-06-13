d6a83af9bdf0b11956aa75ee260cbf9c1b5a3610
Christer-L <christer.lohk@gmail.com>
2022-06-13 10:24:23 +0200

    Add current version of train.py

--- wnet/train.py
+++ wnet/train.py
@@ -20,7 +20,7 @@ from wnet.utils import data, soft_n_cut_loss, ssim, utils
 # Debugging...
 torch.autograd.set_detect_anomaly(True)
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "5,6,7"
+os.environ["CUDA_VISIBLE_DEVICES"] = "6"
 
 # widget list for the progress bar
 widgets = [
@@ -33,12 +33,11 @@ widgets = [
     ") ",
 ]
 
-BASE_PATH = "/scratch/homedirs/clohk/weights/wnet_weights/Data_Eduardo/cell/patched/"
-SAVE_PATH = "saved_models/net.pth"
-TEST_BATCH_SIZE = 1
-# Test image path in get_dataset().
-LOSS = np.inf
+BASE_PATH = "/scratch/homedirs/clohk/Data_Eduardo/cell/patched/" # "/scratch/homedirs/clohk/wnet_pytorch/images/JPEGImages/"   "  
 
+SAVE_PATH = "saved_models/net.pth"
+
+LOSS = np.inf
 
 def save_model(net, loss):
     global LOSS
@@ -48,25 +47,25 @@ def save_model(net, loss):
 
 
 def get_datasets(path_img, config):
-    img_path_list = utils.list_files_path(path_img)[0:5000]
+    img_path_list = utils.list_files_path(path_img)[0:10000]
     img_path_list = utils.shuffle_list(img_path_list)
     # not good if we need to do metrics
     img_train, img_val = sk.train_test_split(
         img_path_list, test_size=0.2, random_state=42
     )
 
-    img_test_dir = "/scratch/homedirs/clohk/weights/wnet_weights/Data_Eduardo/cell/test_set/Images"
-    labels_test_dir = "/scratch/homedirs/clohk/weights/wnet_weights/Data_Eduardo/cell/test_set/Labels"
+    #img_test_dir = "/scratch/homedirs/clohk/weights/wnet_weights/Data_Eduardo/cell/test_set/Images"
+    #labels_test_dir = "/scratch/homedirs/clohk/weights/wnet_weights/Data_Eduardo/cell/test_set/Labels"
 
     dataset_train = data.Unsupervised_dataset(config.batch_size, config.size, img_train)
     dataset_val = data.Unsupervised_dataset(config.batch_size, config.size, img_val)
-    dataset_test = data.Test_dataset(TEST_BATCH_SIZE, config.size, glob(os.path.join(img_test_dir, "*")),
-                                     labels_test_dir)
-    return dataset_train, dataset_val, dataset_test
+    #dataset_test = data.Test_dataset(TEST_BATCH_SIZE, config.size, glob(os.path.join(img_test_dir, "*")),
+    #                                 labels_test_dir)
+    return dataset_train, dataset_val   #, dataset_test
 
 
-def _step(net, step, dataset, optim_enc, optim_glob, epoch, config, ncut, test_dataset):
-    _enc_loss, _recons_loss, _dice = [], [], []
+def _step(net, step, dataset, optim_enc, optim_glob, epoch, config, ncut):
+    _enc_loss, _recons_loss = [], []
     if step == "Train":
         net.train()
     else:
@@ -85,39 +84,17 @@ def _step(net, step, dataset, optim_enc, optim_glob, epoch, config, ncut, test_d
                 enc_loss.backward(retain_graph=True)
                 optim_enc.step()
             mask, recons = net.forward(imgs)
-            glob_loss = nn.MSELoss()(imgs, recons.cuda())
+            glob_loss = nn.MSELoss(reduction='sum')(imgs, recons.cuda())
             if step == "Train":
                 glob_loss.backward()
                 optim_glob.step()
             _enc_loss.append(enc_loss.item())
             _recons_loss.append(glob_loss.item())
 
-            if step == "Validation":
-                _dice = []
-                fig, axs = plt.subplots(len(test_dataset), 3, figsize=(20, 40))
-                for k in range(len(test_dataset)):
-                    img, label = test_dataset[k]
-                    label = torch.squeeze(label, dim=1)
-                    mask = net.enc_forward(img)
-                    argmax = mask.argmax(dim=1) # mask > 0.5
-                    argmax_inv = torch.float_power(argmax - 1, 2)
-
-                    axs[k, 0].imshow(np.array(torch.squeeze(img).cpu() * 255))
-                    axs[k, 1].imshow(np.array(torch.squeeze(label).cpu() * 255))
-                    axs[k, 2].imshow(np.array(torch.squeeze(argmax).cpu() * 255))
-
-                    _dice.append(np.amax(np.array([
-                        jaccard_score(np.array(torch.flatten(label).cpu()), np.array(torch.flatten(argmax).cpu())),
-                        jaccard_score(np.array(torch.flatten(label).cpu()), np.array(torch.flatten(argmax_inv).cpu()))
-                    ])))
-
-                plt.savefig("/scratch/homedirs/clohk/wnet_pytorch/data/epoch_results/test_{}.png".format(epoch))
-                plt.close()
-
             if step == "Validation" and (epoch + 1) == config.epochs:
                 utils.visualize(net, imgs, epoch + 1, i, config,
-                                path="/scratch/homedirs/clohk/wnet_pytorch/data/results")
-    return _enc_loss, _recons_loss, _dice
+                                path="/scratch/homedirs/clohk/wnet_pytorch/data/results_experiment_7classes")
+    return _enc_loss, _recons_loss
 
 
 def train(path_imgs, config, epochs=5):
@@ -126,14 +103,17 @@ def train(path_imgs, config, epochs=5):
 
     optimizer_enc = optim.Adam(net.u_enc.parameters(), lr=config.lr)
     optimizer_glob = optim.Adam(net.parameters(), lr=config.lr)
+    scheduler_enc = optim.lr_scheduler.StepLR(optimizer_enc, step_size=10, gamma=0.1)
+    scheduler_glob = optim.lr_scheduler.StepLR(optimizer_glob, step_size=10, gamma=0.1)
 
-    # Get the dataset paths.
-    dataset_train, dataset_val, dataset_test = get_datasets(path_imgs, config)
     epoch_enc_train = []
     epoch_recons_train = []
     epoch_enc_val = []
     epoch_recons_val = []
-    dice = []
+
+    # Get the dataset paths.
+    dataset_train, dataset_val = get_datasets(path_imgs, config)
+
 
     # Initialize the encoder loss.
     ncut = soft_n_cut_loss.NCutLossOptimized()
@@ -145,8 +125,9 @@ def train(path_imgs, config, epochs=5):
         for step in ["Train", "Validation"]:
             utils.print_gre(step+":")
             dataset = dataset_train if step == "Train" else dataset_val
-            _enc_loss, _recons_loss, _dice = _step(
-                net, step, dataset, optimizer_enc, optimizer_glob, epoch, config, ncut, dataset_test)
+            _enc_loss, _recons_loss = _step(
+                net, step, dataset, optimizer_enc, optimizer_glob, epoch, config, ncut)
+
             if step == "Train":
                 epoch_enc_train.append(np.array(_enc_loss).mean())
                 epoch_recons_train.append(np.array(_recons_loss).mean())
@@ -155,17 +136,23 @@ def train(path_imgs, config, epochs=5):
             else:
                 epoch_enc_val.append(np.array(_enc_loss).mean())
                 epoch_recons_val.append(np.array(_recons_loss).mean())
-                dice.append(np.array(_dice).mean())
-                results = "Encoding loss: {:.9f}\t Reconstruction loss: {:.9f}\t Dice: {:.6f}".format(
-                    np.array(_enc_loss).mean(), np.array(_recons_loss).mean(), np.array(_dice).mean())
+                results = "Encoding loss: {:.9f}\t Reconstruction loss: {:.9f}".format(
+                    np.array(_enc_loss).mean(), np.array(_recons_loss).mean())
 
         utils.print_gre(results)
+        print()
+        print("LR optimizer_enc: {}".format(optimizer_enc.param_groups[0]['lr']))
+        print("LR optimizer_glob: {}".format(optimizer_enc.param_groups[0]['lr']))
+
+        scheduler_enc.step()
+        scheduler_glob.step()
 
     utils.learning_curves(
         epoch_enc_train, epoch_recons_train, epoch_enc_val, epoch_recons_val,
-        path="data/plot.png")
-    plt.plot(dice)
-    plt.savefig("/scratch/homedirs/clohk/wnet_pytorch/data/dice.png")
+        path="data/plot_Eduardos_data_experiment_7classes.png")
+
+    #plt.plot(dice)
+    #plt.savefig("/scratch/homedirs/clohk/wnet_pytorch/data/dice.png")
 
 
 if __name__ == "__main__":
